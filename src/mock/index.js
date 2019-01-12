b3cf28f698e65d91b8361b8c825f090251053032
zhangbin <zb881010>
2019-01-12 22:09:32 +0800

    完成运行参数配置及mock

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,6 +1,7 @@
 import Mock from 'mockjs'
 
 import { login, userInfo, menuList } from './oauth/oauth'
+import { deleteParam, queryParam, updateParam } from './sysconfig/param'
 
 // 配置Ajax请求延时，可用来测试网络延迟大时项目中一些效果
 Mock.setup({
@@ -13,3 +14,6 @@ Mock.mock(/\/oauth\/menulist/, /get/i, menuList)
 Mock.mock(/\/oauth\/userinfo/, /(patch)|(put)/i, function (options) {
   return Object.assign(userInfo(), JSON.parse(options.body))
 })
+Mock.mock(/\/oauth\/param/, /(get)|(put)|(patch)/i, updateParam)
+Mock.mock(/\/oauth\/param/, /delete/i, deleteParam)
+Mock.mock(/\/oauth\/param/, /post/i, queryParam)
---
+++ src/mock/sysconfig/param.js
@@ -0,0 +1,44 @@
+export const updateParam = () => {
+  return {
+    'id': '58e19099-15c5-11e9-95dd-d8c49792dd7b',
+    'name': 'name',
+    'value': 'value',
+    'config_des': 'dnioa你哦按身份111',
+    'enabled': false,
+    'covert': false
+  }
+}
+export const deleteParam = () => {
+  return {
+    message: '删除成功'
+  }
+}
+export const queryParam = () => {
+  return {
+    'content': [{
+      'id': '58e19099-15c5-11e9-95dd-d8c49792dd7b',
+      'name': 'name',
+      'value': 'value',
+      'config_des': 'dnioa你哦按身份',
+      'enabled': true,
+      'covert': false
+    }],
+    'pageable': {
+      'sort': { 'sorted': true, 'unsorted': false, 'empty': false },
+      'offset': 0,
+      'page_number': 0,
+      'page_size': 10,
+      'paged': true,
+      'unpaged': false
+    },
+    'last': true,
+    'total_pages': 1,
+    'total_elements': 1,
+    'size': 10,
+    'number': 0,
+    'sort': { 'sorted': true, 'unsorted': false, 'empty': false },
+    'number_of_elements': 1,
+    'first': true,
+    'empty': false
+  }
+}
