2468c15cbbbea2805dd7470d4329c4d2f2decaac
zhangbin <zb881010>
2019-01-18 01:32:40 +0800

    增加机构配置mock

--- src/mock/index.js
+++ src/mock/index.js
@@ -3,6 +3,8 @@ import Mock from 'mockjs'
 import { login, userInfo, menuList } from './oauth/oauth'
 import { deleteRuntime, queryRuntime, updateRuntime } from './sysconfig/runtime'
 import { deleteApp, queryApp, updateApp } from './sysconfig/application'
+import { getOrgList, getOrgInfo, deleteOrg } from './sysconfig/organization'
+import { getModUserList } from './sysconfig/user'
 
 // 配置Ajax请求延时，可用来测试网络延迟大时项目中一些效果
 Mock.setup({
@@ -21,3 +23,11 @@ Mock.mock(/\/oauth\/runtime/, /post/i, queryRuntime)
 Mock.mock(/\/oauth\/app/, /(get)|(put)|(patch)/i, updateApp)
 Mock.mock(/\/oauth\/app/, /delete/i, deleteApp)
 Mock.mock(/\/oauth\/app/, /post/i, queryApp)
+Mock.mock(/\/oauth\/org\/.*/, /get/i, getOrgInfo)
+Mock.mock(/\/oauth\/org/, /get/i, getOrgList)
+Mock.mock(/\/oauth\/org/, /put/i, getOrgInfo)
+Mock.mock(/\/oauth\/org/, /patch/i, function (options) {
+  return Object.assign(getOrgInfo(), JSON.parse(options.body))
+})
+Mock.mock(/\/oauth\/app/, /delete/i, deleteOrg)
+Mock.mock(/\/oauth\/moduserlist/, /get/i, getModUserList)
---
+++ src/mock/sysconfig/user.js
@@ -0,0 +1,14 @@
+export const getModUserList = () => {
+  return [{
+    'id': '123',
+    'name': 'test',
+    'loginno': 'lll',
+    'mobile': '111',
+    'levels': 1,
+    'enabled': true,
+    'sort': 1,
+    'organization_set': [],
+    'organization_mng_set': [],
+    'role_set': []
+  }]
+}
