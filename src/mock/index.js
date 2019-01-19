c5fa3fb197a5d47725bbd82e6e8ca674be2e1343
zhangbin <zb881010>
2019-01-19 22:40:25 +0800

    完成角色配置mock数据

--- src/mock/index.js
+++ src/mock/index.js
@@ -2,9 +2,11 @@ import Mock from 'mockjs'
 
 import { login, userInfo, menuList } from './oauth/oauth'
 import { deleteRuntime, queryRuntime, updateRuntime } from './sysconfig/runtime'
-import { deleteApp, queryApp, updateApp } from './sysconfig/application'
+import { getAppList, deleteApp, queryApp, updateApp } from './sysconfig/application'
 import { getOrgList, getOrgInfo, deleteOrg } from './sysconfig/organization'
 import { getModUserList } from './sysconfig/user'
+import { getRoleCodeList, getRoleList, getRoleInfo, updateRole, deleteRole } from './sysconfig/role'
+import { getMenuListByAppId, getModuleFuncListByAppId } from './sysconfig/auth'
 
 // 配置Ajax请求延时，可用来测试网络延迟大时项目中一些效果
 Mock.setup({
@@ -20,7 +22,9 @@ Mock.mock(/\/oauth\/userinfo/, /(patch)|(put)/i, function (options) {
 Mock.mock(/\/oauth\/runtime/, /(get)|(put)|(patch)/i, updateRuntime)
 Mock.mock(/\/oauth\/runtime/, /delete/i, deleteRuntime)
 Mock.mock(/\/oauth\/runtime/, /post/i, queryRuntime)
-Mock.mock(/\/oauth\/app/, /(get)|(put)|(patch)/i, updateApp)
+Mock.mock(/\/oauth\/app/, /(put)|(patch)/i, updateApp)
+Mock.mock(/\/oauth\/app\/updatesecret/, /get/i, updateApp)
+Mock.mock(/\/oauth\/app/, /get/i, getAppList)
 Mock.mock(/\/oauth\/app/, /delete/i, deleteApp)
 Mock.mock(/\/oauth\/app/, /post/i, queryApp)
 Mock.mock(/\/oauth\/org\/.*/, /get/i, getOrgInfo)
@@ -31,3 +35,13 @@ Mock.mock(/\/oauth\/org/, /patch/i, function (options) {
 })
 Mock.mock(/\/oauth\/org/, /delete/i, deleteOrg)
 Mock.mock(/\/oauth\/moduserlist/, /get/i, getModUserList)
+Mock.mock(/\/oauth\/role\/rolecodes/, /get/i, getRoleCodeList)
+Mock.mock(/\/oauth\/role\/.*/, /get/i, getRoleInfo)
+Mock.mock(/\/oauth\/role/, /get/i, getRoleList)
+Mock.mock(/\/oauth\/role/, /put/i, updateRole)
+Mock.mock(/\/oauth\/role/, /patch/i, function (options) {
+  return Object.assign(updateRole(), JSON.parse(options.body))
+})
+Mock.mock(/\/oauth\/role/, /delete/i, deleteRole)
+Mock.mock(/\/oauth\/auth\/menulist\/.*/, /get/i, getMenuListByAppId)
+Mock.mock(/\/oauth\/auth\/modulefunclist\/.*/, /get/i, getModuleFuncListByAppId)
---
+++ src/mock/sysconfig/role.js
@@ -0,0 +1,55 @@
+export const getRoleCodeList = () => {
+  return ['OTHER', 'ADMIN', 'TEST']
+}
+export const getRoleList = () => {
+  return [{
+    'id': 'dbe88dbc-19fb-11e9-894e-d8c49792dd7b',
+    'appid': 'dbd1458a-19fb-11e9-894e-d8c49792dd7b',
+    'name': '超级管理员',
+    'code': 'ADMIN',
+    'levels': 0,
+    'sort': 0
+  }, {
+    'id': 'b09d505f-1bf2-11e9-a49e-d8c49792dd7b',
+    'appid': 'aa4da240-1bf2-11e9-a49e-d8c49792dd7b',
+    'name': '新角色',
+    'code': 'OTHER',
+    'levels': 999,
+    'sort': 1
+  }, {
+    'id': 'fd3bb9d8-1bf1-11e9-a49e-d8c49792dd7b',
+    'appid': 'dbd1458a-19fb-11e9-894e-d8c49792dd7b',
+    'name': '测试人员',
+    'code': 'TEST',
+    'levels': 1,
+    'sort': 1
+  }]
+}
+export const getRoleInfo = () => {
+  return {
+    'id': 'fd3bb9d8-1bf1-11e9-a49e-d8c49792dd7b',
+    'appid': 'dbd1458a-19fb-11e9-894e-d8c49792dd7b',
+    'name': '测试人员',
+    'code': 'TEST',
+    'levels': 1,
+    'sort': 1,
+    'user_ids': [],
+    'menu_ids': ['dbdc0cd5-19fb-11e9-894e-d8c49792dd7b', 'dbdd18fc-19fb-11e9-894e-d8c49792dd7b', 'dbdc4e5a-19fb-11e9-894e-d8c49792dd7b', 'dbdba3bf-19fb-11e9-894e-d8c49792dd7b', 'dbdc90b0-19fb-11e9-894e-d8c49792dd7b', 'dbdcd7a2-19fb-11e9-894e-d8c49792dd7b'],
+    'module_func_ids': []
+  }
+}
+export const updateRole = () => {
+  return {
+    'id': 'fd3bb9d8-1bf1-11e9-a49e-d8c49792dd7b',
+    'appid': 'dbd1458a-19fb-11e9-894e-d8c49792dd7b',
+    'name': '测试人员',
+    'code': 'TEST',
+    'levels': 1,
+    'sort': 1
+  }
+}
+export const deleteRole = () => {
+  return {
+    message: '删除成功'
+  }
+}
