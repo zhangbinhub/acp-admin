8191613c9cf44f73941ee7f831b94d3273552ec8
zhangbin1010 <391289241@qq.com>
2019-08-26 23:05:41 +0800

    [Upgrade] 增加项目部署路径配置，支持部署至非根路径

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@
 // 官方vue.config.js 参考文档 https://cli.vuejs.org/zh/config/#css-loaderoptions
 // 这里只列一部分，具体配置参考文档
 module.exports = {
-  publicPath: '/', // 部署应用时的根路径(默认'/'),也可用相对路径(存在使用限制)
+  publicPath: '/', // 部署应用时的根路径(默认'/'),也可用相对路径(当使用相对路径时，router中也需要配置base为对应的路径)
   outputDir: 'dist', // 运行时生成的生产环境构建文件的目录(默认''dist''，构建之前会被清除)
   assetsDir: 'public', // 放置生成的静态资源(s、css、img、fonts)的(相对于 outputDir 的)目录(默认'')
   indexPath: 'index.html', // 指定生成的 index.html 的输出路径(相对于 outputDir)也可以是一个绝对路径。
