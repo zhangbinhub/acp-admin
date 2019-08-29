5ddb12d7179bd42f94759607802a957bd90f29e0
zhangbin1010 <391289241@qq.com>
2019-08-29 12:30:23 +0800

    [Upgrade] 修改自定义npm脚本名称

--- src/components/views/demo/Demo4.vue
+++ src/components/views/demo/Demo4.vue
@@ -1,10 +1,7 @@
 <template>
-  <div>
-    <label>
-      <Input type="text"/>
-    </label>
-    <p>demo4,name={{this.$route.query.name}}</p>
-  </div>
+  <Card>
+    <iframe src='http://localhost:8080/demo4?name=123' style="min-height:800px; width: 100%;border: 0"></iframe>
+  </Card>
 </template>
 <script>
     export default {
